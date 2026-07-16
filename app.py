74d59c335403daf8762b12f64c9b56118bb6e31a
Rifa Adritya P <rifa.pamungkas57@gmail.com>
2026-07-16 23:28:11 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from sklearn.datasets import load_iris
 from sklearn.ensemble import RandomForestClassifier
 
 st.title("🌸 Iris Flower Classification App")
-st.subheader("Tugas Sesi 14 : Mochamad Radmdhan Irawan")
+st.subheader("Tugas Sesi 14 : Rifa Adritya Pamungkas")
 
 # Load and cache data
 @st.cache_data
